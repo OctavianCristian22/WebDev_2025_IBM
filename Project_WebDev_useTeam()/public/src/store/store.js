addaa8ead6db68c4429f5f070ab33c1c5033cac7
OctavianCristian22 <octaviancristianedutanu@gmail.com>
2025-07-05 12:45:30 +0300

    Add files via upload

---
+++ Project_WebDev_useTeam()/public/src/store/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit';
+import playerReducer from '../reducers/index';
+
+const store = configureStore({ reducer: playerReducer });
+
+export default store;
