77523bb0a2673332ad9f0f8085f46e5893b086b8
OctavianCristian22 <octaviancristianedutanu@gmail.com>
2025-09-07 13:51:58 +0300

    Update App.js

--- Project_WebDev_useTeam()/src/App.js
+++ Project_WebDev_useTeam()/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import ArrowKeys from './components/arrow-keys/arrow-keys';
 import MapBase from './components/map-base/map-base';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <MapBase/>
-        <ArrowKeys/>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <div className="header1">Edutanu Octavian Cristian</div>
+          <div className="header2">My application</div>
+        </header>
+
+        <main className="App-body">
+          <MapBase />
+          <ArrowKeys />
+        </main>
+      </div>
+    </Provider>
   );
 }
 
