dc161f72e6c0b8776dbb235cc3ab8f681fb0d15a
OctavianCristian22 <octaviancristianedutanu@gmail.com>
2025-09-07 13:48:22 +0300

    Update constante.js

--- Project_WebDev_useTeam()/src/constants/constante.js
+++ Project_WebDev_useTeam()/src/constants/constante.js
@@ -1,1 +1,5 @@
-export const GRID_SIZE = 10;
+import { generateStructuredMap } from "../utils/utils";
+export const GRID_SIZE = 100;
+export const TILE_SIZE_PX = 50;
+export const VIEWPORT_SIZE = 10;
+export const SOLIDS_MATRIX = generateStructuredMap();
